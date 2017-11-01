d7f1c78142ece329328a89c8f49f5b102b6f8ffe
Alan Tedeev <tedious.alan@outlook.com>
2017-11-01 19:33:06 +0300

    change all we need for exercise 2

--- app/src/main/java/com/loftblog/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftblog/moneytracker/MainActivity.java
@@ -2,6 +2,11 @@ package com.loftblog.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      final EditText titleEdit = findViewById(R.id.name);
+      final ImageButton addButton = findViewById(R.id.add);
+      final EditText titleEdit2=findViewById(R.id.name2);
+
+        titleEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(titleEdit.getText().length()!=0&& titleEdit2.getText().length()!=0)
+                    addButton.setEnabled(!TextUtils.isEmpty(s));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        titleEdit2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addButton.setEnabled(!TextUtils.isEmpty(s));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
     }
 
     @Override
